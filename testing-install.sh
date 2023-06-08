60946cad54ef7d260f00d5178602fd971cd83039
Tushantverma <108903932+Tushantverma@users.noreply.github.com>
2023-06-09 02:03:21 +0530

    added comment for information to scroll back on tty

--- testing-install.sh
+++ testing-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #setfont ter-128n   #### changing the font size "ter" is a font name and "128n" is the font size
+#screen -h 99999    #### for scrollback on tty while installing OS (99999 is the schrollback buffer)
+
 #part11
 echo "##########################################################################"
 echo "################### checking internet connection #########################"
