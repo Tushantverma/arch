473f0153a0ff01aad29120b2fbe865621683a6fc
TushantVerma <Tushantverma@users.noreply.github.com>
2023-12-08 00:44:00 +0530

    added chaotic aur

--- testing-install.sh
+++ testing-install.sh
@@ -358,10 +358,7 @@ echo "########### setting up /etc/pacman.conf (for Main System) ################
 echo "##########################################################################"
 
 setup_pacman_conf # executing exported function to setup pacman.conf for main system
-
 pacman -Syyy
-
-
 
 
 
@@ -377,7 +374,26 @@ rm -rf arcolinux-spices
 # source :- https://www.arcolinux.info/arcolinux-spices-application/
 
 
+echo "##########################################################################"
+echo "######################## installing Chaotic AUR ##########################"
+echo "##########################################################################"
+##-----------------------------------------------------------------------------##
+pacman-key --recv-key  3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
 
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  
+##------------------------------ OR (uncomment one)----------------------------##
+# pacman -S arcolinux_repo_3party/chaotic-keyring
+# pacman -S arcolinux_repo_3party/chaotic-mirrorlist
+##-----------------------------------------------------------------------------##
+
+echo "
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+pacman -Syyy
+# source https://aur.chaotic.cx/
 
 
 echo "##########################################################################"
